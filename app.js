cb76ab5376c95db3e99e7277ff97bfd297e827f8
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-13 21:52:54 +0000

    Add functionality for logging-in user and managing user session

--- app.js
+++ app.js
@@ -40,9 +40,46 @@ app.use(helmet());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Try to find user credentials in database
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            // Match user
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            // If user found, match hashed password
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+// Manage user session
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -51,18 +88,18 @@ app.use(compression());
 app.use("/", indexRouter);
 app.use("/messages", messagesRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
+    // Set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
+    // Render the error page
     res.status(err.status || 500);
     res.render("error");
 });
