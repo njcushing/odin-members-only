711207f4588e674bf58c4db1423fabf04ce7c91c
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-14 20:25:46 +0000

    Redirect user to sign-in instead of sign-up at homepage

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
     if (!req.user) {
-        res.redirect("/sign-up");
+        res.redirect("/sign-in");
     }
     res.render("index");
 });
