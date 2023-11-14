eeb0bb6d833429363e989dc59376ed63f8943e9b
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-14 20:49:56 +0000

    Set up / GET route for sign-up

--- routes/signup.js
+++ routes/signup.js
@@ -1,4 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+const signupController = require("../controllers/signupController");
+
+router.get("/", signupController.signupGet);
+router.post("/", signupController.signupPost);
+
 module.exports = router;
