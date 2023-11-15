5dda2aa903baaaeaabeaae6fc00cebefbc90b25a
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-15 20:59:14 +0000

    Remove automatic redirect from homepage to sign-in page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,5 @@
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-    if (!req.user) {
-        res.redirect("/sign-in");
-    }
     res.render("index");
 });
