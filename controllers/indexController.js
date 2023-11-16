126333724fd3fad06ace02447e7e58543e612fed
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-16 11:01:08 +0000

    Query Message model to find all messages for homepage view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require("express-async-handler");
 
+const Message = require("../models/message");
+
 exports.index = asyncHandler(async (req, res, next) => {
-    res.render("index");
+    const messages = await Message.find().populate("author").exec();
+    res.render("index", { messages: messages });
 });
