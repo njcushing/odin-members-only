2f984169227a009fbb46d64a18c91bfb7a8705ec
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-16 12:01:05 +0000

    Set up functionality for /membership POST route

--- controllers/membershipController.js
+++ controllers/membershipController.js
@@ -1,8 +1,38 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
+const User = require("../models/user");
+
 exports.membershipGet = asyncHandler(async (req, res, next) => {
     res.render("membership");
 });
 
-exports.membershipPost = asyncHandler(async (req, res, next) => {});
+exports.membershipPost = [
+    body("password")
+        .trim()
+        .escape()
+        .custom((value, { req, loc, path }) => {
+            if (value !== process.env.MEMBER_PASSCODE) {
+                throw new Error("That password is incorrect! Try again.");
+            } else {
+                return value;
+            }
+        }),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!res.locals.currentUser) res.render("newmessage_form");
+        if (!errors.isEmpty()) {
+            res.render("membership", { errors: errors.array() });
+        } else {
+            const updatedUser = await User.findByIdAndUpdate(
+                res.locals.currentUser._id,
+                {
+                    member: true,
+                },
+                {}
+            );
+            if (!updatedUser) res.render("membership");
+            else res.redirect("/");
+        }
+    }),
+];
