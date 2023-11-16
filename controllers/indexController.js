ee19ea53fa7294e95e90e0f280d8dafeacc58a34
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-16 11:22:25 +0000

    Sort messages in date_posted order; most recent messages first

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,6 +3,9 @@ const asyncHandler = require("express-async-handler");
 const Message = require("../models/message");
 
 exports.index = asyncHandler(async (req, res, next) => {
-    const messages = await Message.find().populate("author").exec();
+    const messages = await Message.find()
+        .populate("author")
+        .sort({ date_posted: -1 })
+        .exec();
     res.render("index", { messages: messages });
 });
