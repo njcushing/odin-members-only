0b4843c854d4bd413764cb8762c542fcf9ab5d46
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-16 12:27:50 +0000

    Write functionality for /administrator POST route

--- controllers/administratorController.js
+++ controllers/administratorController.js
@@ -3,6 +3,36 @@ const { body, validationResult } = require("express-validator");
 
 const User = require("../models/user");
 
-exports.administratorGet = asyncHandler(async (req, res, next) => {});
+exports.administratorGet = asyncHandler(async (req, res, next) => {
+    res.render("administrator");
+});
 
-exports.administratorPost = [asyncHandler(async (req, res, next) => {})];
+exports.administratorPost = [
+    body("password")
+        .trim()
+        .escape()
+        .custom((value, { req, loc, path }) => {
+            if (value !== process.env.ADMIN_PASSCODE) {
+                throw new Error("That password is incorrect! Try again.");
+            } else {
+                return value;
+            }
+        }),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!res.locals.currentUser) res.render("administrator");
+        if (!errors.isEmpty()) {
+            res.render("administrator", { errors: errors.array() });
+        } else {
+            const updatedUser = await User.findByIdAndUpdate(
+                res.locals.currentUser._id,
+                {
+                    admin: true,
+                },
+                {}
+            );
+            if (!updatedUser) res.render("administrator");
+            else res.redirect("/");
+        }
+    }),
+];
